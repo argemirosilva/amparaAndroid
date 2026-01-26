bbc94c22834e8f9641868cdd0436a9d0d9273ad7
argemirosilva <65288540+argemirosilva@users.noreply.github.com>
2026-01-25 20:11:54 -0500

    Fix: Remove Lovable URL and set correct appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.9eae17f51a4049eabcb6887a2af1fbbc',
+  appId: 'tech.orizon.ampara',
   appName: 'AMPARA',
   webDir: 'dist',
   server: {
-    url: 'https://9eae17f5-1a40-49ea-bcb6-887a2af1fbbc.lovableproject.com?forceHideBadge=true',
-    cleartext: true
+    androidScheme: 'https'
   },
   plugins: {
     SplashScreen: {
